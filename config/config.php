cc6c513ff999286b99128144c66d016c169e9b1d
penelopenastou <penelope_nastou@hotmail.com>
2020-05-21 14:57:35 +0300

    Define env vars

--- config/config.php
+++ config/config.php
@@ -1,11 +1,12 @@
 <?php
 //set database required global constants
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'test_employee_portal');
+define('DB_SERVER', $_ENV['DB_SERVER']);
+define('DB_USERNAME', $_ENV['DB_USERNAME']);
+define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
+define('DB_NAME', $_ENV['DB_NAME']);
+
 //set the project's root path
-define('PROJECT_ROOT','localhost/employee_portal/public');
+define('PROJECT_ROOT',$_ENV['PROJECT_ROOT']);
 
 //connect database
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD,DB_NAME);
