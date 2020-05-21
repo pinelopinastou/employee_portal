92175ef3645d57272c5ad74ac61da300c2469d79
penelopenastou <penelope_nastou@hotmail.com>
2020-05-21 18:14:00 +0300

    Imrpove mail format

--- src/services/mails_manager.php
+++ src/services/mails_manager.php
@@ -3,12 +3,13 @@ class MailsManager{
   
   //send mail to administrator
   static function send_request_for_approval_to($email,$name,$date_from,$date_to,$reason,$approve_link,$reject_link){
-    $message = "<p> Dear supervisor, employee {$name} requested for some time off, starting on {$date_from} and ending on {$date_to}, stating the reason: {$reason}. Click on one of the below links to approve or reject the application: <a href={$approve_link}> approve </a> - <a href={$reject_link}> reject </a> </p>";
+    $message = "<p> Dear supervisor, <br> Employee {$name} requested for some time off, starting on {$date_from} and ending on {$date_to}, stating the reason: {$reason}.<br> Click on one of the below links to approve or reject the application: <a href={$approve_link}> approve </a> - <a href={$reject_link}> reject </a> </p>";
     $headers = 'From: employee_portal@example.com' . "\r\n" .
     'Reply-To: employee_portal@example.com' . "\r\n" .
+    'Content-type: text/html; charset=iso-8859-1' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-  	$success = mail($email, 'A request in pending you approval', $message);
+  	$success = mail($email, 'A request in pending you approval', $message, $headers);
     if (!$success) {
       echo $errorMessage = error_get_last()['message'];
     }
@@ -16,12 +17,13 @@ class MailsManager{
 
   //send mail after approval/rejection
   static function send_request_response($email,$status,$created_at){
-    $message = "Dear employee, your supervisor has {$status} your application submitted on {$created_at}.";
+    $message = "<p>Dear employee, <br> Your supervisor has {$status} your application submitted on {$created_at}.</p>";
     $headers = 'From: employee_portal@example.com' . "\r\n" .
     'Reply-To: employee_portal@example.com' . "\r\n" .
+    'Content-type: text/html; charset=iso-8859-1' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
         
-  	$success = mail($email,"Your request has been {$status}", $message);
+  	$success = mail($email,"Your request has been {$status}", $message, $headers);
     if (!$success) {
       echo $errorMessage = error_get_last()['message'];
     }
