ab2db361d01822b8317beb9927a547f23977282e
penelopenastou <penelope_nastou@hotmail.com>
2020-05-21 16:13:03 +0300

    Env variables configuration

--- config/config.php
+++ config/config.php
@@ -1,13 +1,29 @@
 <?php
+//load env variables
+if(file_exists('../.env.php')) {
+      include '../.env.php';
+  }
+  if(!function_exists('env')) {
+      function env($key, $default = null)
+      {
+          $value = getenv($key);
+
+          if ($value === false) {
+              return $default;
+          }
+
+          return $value;
+      }
+  }
+
 //set database required global constants
-define('DB_SERVER', $_ENV['DB_SERVER']);
-define('DB_USERNAME', $_ENV['DB_USERNAME']);
-define('DB_PASSWORD', $_ENV['DB_PASSWORD']);
-define('DB_NAME', $_ENV['DB_NAME']);
+define('DB_SERVER', env('DB_SERVER'));
+define('DB_USERNAME', env('DB_USERNAME'));
+define('DB_PASSWORD', env('DB_PASSWORD'));
+define('DB_NAME', env('DB_NAME'));
 
 //set the project's root path
-define('PROJECT_ROOT',$_ENV['PROJECT_ROOT']);
-
+define('PROJECT_ROOT',env('PROJECT_ROOT'));
 //connect database
 $conn = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD,DB_NAME);
 if ($conn->connect_error) {
