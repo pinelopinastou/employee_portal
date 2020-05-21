5c3bf5e28a4919360fea2cc28a276da406baa744
penelopenastou <penelope_nastou@hotmail.com>
2020-05-21 14:13:30 +0300

    Add html tags to message

--- src/services/mails_manager.php
+++ src/services/mails_manager.php
@@ -3,7 +3,7 @@ class MailsManager{
   
   //send mail to administrator
   static function send_request_for_approval_to($email,$name,$date_from,$date_to,$reason,$approve_link,$reject_link){
-    $message = "Dear supervisor, employee {$name} requested for some time off, starting on {$date_from} and ending on {$date_to}, stating the reason: {$reason}. Click on one of the below links to approve or reject the application: {$approve_link} - {$reject_link}";
+    $message = "<p> Dear supervisor, employee {$name} requested for some time off, starting on {$date_from} and ending on {$date_to}, stating the reason: {$reason}. Click on one of the below links to approve or reject the application: <a href={$approve_link}> approve </a> - <a href={$reject_link}> reject </a> </p>";
     $headers = 'From: employee_portal@example.com' . "\r\n" .
     'Reply-To: employee_portal@example.com' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
